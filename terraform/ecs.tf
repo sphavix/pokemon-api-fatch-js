1a63245a7e8e1ca2d0f92b25af69e3f5db298f42
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-06-11 10:36:53 +0200

    Fixed Spelling Error in ECS Service resource

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,24 +2,6 @@ resource "aws_ecs_cluster" "ecs" {
   name = "pokemone_cluster"
 }
 
-resource "aws_ecs_service" "service" {
-  name = "pokemone_service"
-  cluster = aws_ecs_cluster.ecs.arn
-  launch_type = "FARGATE"
-  enable_execute_command = true
-
-  deployment_maximum_percent = 200
-  deployment_minimum_healthy_percent = 100
-  desired_count = 1
-  task_definition = aws_ecs_task_defition.td.arn
-
-  network_configuration {
-    assign_public_ip = true
-    security_groups = [aws_security_group.sg.arn]
-    subnets = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
-  }
-}
-
 resource "aws_ecs_task_definition" "td" {
   container_definitions = jsonencode([
     {
@@ -44,4 +26,22 @@ resource "aws_ecs_task_definition" "td" {
   network_mode = "awsvpc"
   task_role_arn = "arn:aws:iam::533267023956:role/ecsTaskExecutionRole"
   execution_role_arn = "arn:aws:iam::533267023956:role/ecsTaskExecutionRole"
+}
+
+resource "aws_ecs_service" "service" {
+  name = "pokemone_service"
+  cluster = aws_ecs_cluster.ecs.arn
+  launch_type = "FARGATE"
+  enable_execute_command = true
+
+  deployment_maximum_percent = 200
+  deployment_minimum_healthy_percent = 100
+  desired_count = 1
+  task_definition = aws_ecs_task_definition.td.arn
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups = [aws_security_group.sg.arn]
+    subnets = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
+  }
 }
